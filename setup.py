82ce48ca2ff831b246b6b7c45d8b8e1dc60d3b0a
Cycloctane <Cycloctane@outlook.com>
2025-09-28 20:17:07 +0800

    set symbol visibility to hidden

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-from os import environ
-from os.path import join
+from os import environ, path
 
 from Cython.Build import cythonize
 from setuptools import Extension, setup
+from distutils.ccompiler import get_default_compiler
 
-LIBCUE_SRC = [
-    "cd.c", "cdtext.c", "rem.c", "time.c",
-    "cue_parser.c", "cue_scanner.c"
-]
-
-LIBCUE_PATH = environ.get("LIBCUE_PATH", join("vendor", "libcue"))
+LIBCUE_SRC = ("cd.c", "cdtext.c", "rem.c", "time.c", "cue_parser.c", "cue_scanner.c")
+LIBCUE_PATH = environ.get("LIBCUE_PATH", path.join("vendor", "libcue"))
 
 extensions = [
     Extension(
         "pylibcue._cue",
-        ["pylibcue/_cue.pyx", *[join(LIBCUE_PATH, i) for i in LIBCUE_SRC]],
+        ["pylibcue/_cue.pyx", *(path.join(LIBCUE_PATH, i) for i in LIBCUE_SRC)],
         include_dirs=[LIBCUE_PATH],
-        extra_compile_args=["-DLIBCUE_QUIET_MODE"],
+        define_macros=[("LIBCUE_QUIET_MODE", None)],
+        extra_compile_args=(
+            ["-fvisibility=hidden"] if get_default_compiler() != "msvc" else []
+        ),
+        language="c",
     )
 ]
 
