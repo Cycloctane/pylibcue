3cc259b592ebf60c9cb1db102a39d236d9d978b2
Cycloctane <Cycloctane@outlook.com>
2025-12-05 01:45:06 +0800

    fix Rem not exported

--- pylibcue/__init__.py
+++ pylibcue/__init__.py
@@ -1,11 +1,12 @@
 __version__ = "0.5.1"
 
-from ._cue import Cd, CDText, Track, parse_file, parse_str
+from ._cue import Cd, CDText, Rem, Track, parse_file, parse_str
 from .mode import TrackFlag, TrackMode
 
 __all__ = (
     "Cd",
     "CDText",
+    "Rem",
     "Track",
     "TrackFlag",
     "TrackMode",
